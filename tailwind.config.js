9a4365091375dd540d2fc26793447942b2918d0f
Hfn <titocs845@gmail.com>
2022-07-29 09:38:55 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}", "index.html", "./public/**/*.{html,js}"],
+    theme: {
+      extend: {
+        'colors': {
+            'main-color' : '#F25C05',
+        },
+      },
+    },
+    plugins: [],
+  }
